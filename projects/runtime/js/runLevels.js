d32bbfddc8ff76487d3dc3deeda42117f569e6f6
charlieb122 <Betz@stcharles.k12.la.us>
2023-05-18 19:07:36 +0000

    adding runtime

--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -18,8 +18,52 @@ var runLevels = function (window) {
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
+function createSawBlade(x, y) {
+    var hitZoneSize = 25;
+    var damageFromObstacle = 10;
+    var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+    sawBladeHitZone.x = x;
+    sawBladeHitZone.y = y;
+    game.addGameItem(sawBladeHitZone);
+    var obstacleImage = draw.bitmap("img/sawblade.png");
+    sawBladeHitZone.addChild(obstacleImage);
+    obstacleImage.x = -25;
+    obstacleImage.y = -25;
+}
+createSawBlade(1200, 300);
+createSawBlade(1300, 400);
+createSawBlade(1300, 100);
 
-    
+var enemy = game.createGameItem("enemy", 25);
+var redSquare = draw.rect(50, 50, "red");
+redSquare.x = -25;
+redSquare.y = -25;
+enemy.addChild(redSquare);
+enemy.x = 400;
+enemy.y = groundY - 50;
+game.addGameItem(enemy);
+enemy.velocityX = -1;
+enemy.velocityY = 0;
+
+enemy.onPlayerCollision = function () {
+  game.changeIntegrity(-10)
+};
+
+enemy.onProjectileCollision = function () {
+  game.increaseScore(100);
+enemy.fadeOut();
+};
+
+function createEnemy(x, y) {
+createEnemy(400, groundY - 10);
+createEnemy(800, groundY - 100);
+createEnemy(1200, groundY - 50);
+}
+
+function createReward(){
+
+};
+
 
     function startLevel() {
       // TODO 13 goes below here
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -29,8 +29,9 @@ var background = function (window) {
         // ANIMATION VARIABLES HERE //////////////////////////////////////
         //////////////////////////////////////////////////////////////////
         // TODO (several):
-      
-      
+      var trees = [];
+      var buildings = [];
+
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -39,18 +40,49 @@ var background = function (window) {
             // TODO 1:
             // this currently fills the background with an obnoxious yellow;
             // you should modify both the height and color to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,canvasHeight, '#403d4a');
             background.addChild(backgroundFill);
             
             // TODO 2: - Add a moon and starfield
-            
+            var moon = draw.bitmap("img/moonpng.png");
+        moon.x = 1000;
+        moon.y = 20;
+        moon.scaleX = .30;
+        moon.scaleY = .30;
+        background.addChild(moon);
+
+        for(var i = 0; i < 100; i++){
+            var circle = draw.circle(1, "white", "LightGray", 2);
+        circle.x = canvasWidth * Math.random();
+        circle.y = groundY * Math.random();
+        background.addChild(circle);
+        }
             
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
-            
+            var grass = draw.rect(canvasWidth, groundY, '#1a4538');
+            grass.y = groundY - 40;
+            background.addChild(grass)
+
+        for (var i = 0; i < 8; ++i) {
+            var building = draw.bitmap("img/treepng.png");
+            building.x = 200 * i;
+            building.y = groundY - 380;
+            background.addChild(building);
+            buildings.push(building);
+        }
+        
+
             // TODO 3: Part 1 - Add a tree
-            
-            
+        for(var i = 0; i < 8; i++){
+            var tree = draw.bitmap("img/treepng.png");
+            tree.y = groundY - 360;
+            tree.x = 200 * i;
+            background.addChild(tree);
+            trees.push(tree);
+        }
+        
+        
+
         } // end of render function - DO NOT DELETE
         
         
@@ -63,10 +95,29 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 3: Part 2 - Move the tree!
-            
+       // tree.x = tree.x - 1;
+        
+        //if (tree.x < -200) {
+        //tree.x = canvasWidth;
+        //}
             
             // TODO 4: Part 2 - Parallax
-            
+            for(var i = 0; i < trees.length; i++){
+                var tree1 = trees[i];
+                var edge = tree1.x + 220;
+                tree1.x -= .3;
+                if(edge < 0) {
+                    tree1.x = canvasWidth;
+                }
+            }
+            for(var i = 0; i < buildings.length; i++) {
+                var building1 = buildings[i];
+                var edge = building1.x + 220;
+                building1.x -= .1;
+                if(edge < 0) {
+                    building1.x = canvasWidth;
+                }
+            }
 
         } // end of update function - DO NOT DELETE
         
